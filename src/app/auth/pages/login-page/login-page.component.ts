cce649f3d2d33a990814d5465b3c245b13ebb1eb
JohnyVB <johnyvgoog@gmail.com>
2023-12-11 15:39:51 -0500

    diseño y funcionalidad Login

--- src/app/auth/pages/login-page/login-page.component.ts
+++ src/app/auth/pages/login-page/login-page.component.ts
@@ -1,9 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.css'
 })
 export class LoginPageComponent {
+
+  private fb = inject(FormBuilder);
+
+  public loginForm: FormGroup = this.fb.group(
+    {
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    }
+  );
+
+  login(): void {
+    console.log(this.loginForm.value);
+  }
 
 }
