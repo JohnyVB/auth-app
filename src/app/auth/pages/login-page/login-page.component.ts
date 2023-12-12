358272546f75b73a636d869572816c484239ba24
JohnyVB <johnyvgoog@gmail.com>
2023-12-12 17:01:38 -0500

    guard y chequeo de autenticacion

--- src/app/auth/pages/login-page/login-page.component.ts
+++ src/app/auth/pages/login-page/login-page.component.ts
@@ -2,6 +2,7 @@ import { Component, inject } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { AuthService } from '../../services/auth.service';
 import Swal from 'sweetalert2';
+import { Router } from '@angular/router';
 
 @Component({
   templateUrl: './login-page.component.html',
@@ -11,11 +12,12 @@ export class LoginPageComponent {
 
   private fb = inject(FormBuilder);
   private authService = inject(AuthService);
+  private router = inject(Router);
 
   public loginForm: FormGroup = this.fb.group(
     {
-      email: ['', [Validators.required, Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$")]],
-      password: ['', [Validators.required, Validators.minLength(6)]]
+      email: ['johny3@google.com', [Validators.required, Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$")]],
+      password: ['123456', [Validators.required, Validators.minLength(6)]]
     }
   );
 
@@ -23,7 +25,10 @@ export class LoginPageComponent {
     const { email, password } = this.loginForm.value;
     this.authService.login(email, password)
       .subscribe({
-        next: () => console.log('Todo salio bien'),
+        next: () => {
+          const last_url = localStorage.getItem('last_url');
+          this.router.navigateByUrl(last_url ? last_url : '/dashboard');
+        },
         error: msg => {
           Swal.fire('Error', msg, 'error');
         }
--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -1,10 +1,11 @@
 import { Injectable, computed, inject, signal } from '@angular/core';
 import { environment } from '../../../environments/environments';
-import { HttpClient } from '@angular/common/http';
-import { Observable, catchError, map, tap, throwError } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, catchError, map, of, tap, throwError } from 'rxjs';
 import { User } from '../interfaces/user.interface';
 import { AuthStatus } from '../interfaces/auth-status.enum';
 import { LoginResponse } from '../interfaces/login-response.interface';
+import { CheckTokenResponse } from '../interfaces/checktoken-response.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -21,19 +22,40 @@ export class AuthService {
 
   constructor() { }
 
+  private setAuth(user: User, token: string): boolean {
+    this._currentUser.set(user);
+    this._authStatus.set(AuthStatus.authenticated);
+    localStorage.setItem('authAppToken', token);
+    return true;
+  }
+
   login(email: string, password: string): Observable<boolean> {
     const url = `${this.baseUrl}/auth/login`;
     const body = { email, password };
 
     return this.http.post<LoginResponse>(url, body)
       .pipe(
-        tap(({ user, token }) => {
-          this._currentUser.set(user);
-          this._authStatus.set(AuthStatus.authenticated);
-          localStorage.setItem('authAppToken', token);
-        }),
-        map(() => true),
+        map(({ user, token }) => this.setAuth(user, token)),
         catchError(({ error }) => throwError(() => error.message))
+      );
+  }
+
+  checkAuthStatus(): Observable<boolean> {
+    const url = `${this.baseUrl}/auth/check-token`;
+    const token = localStorage.getItem('authAppToken');
+
+    if (!token) return of(false);
+
+    const headers = new HttpHeaders()
+      .set('Authorization', `Bearer ${token}`);
+
+    return this.http.get<CheckTokenResponse>(url, { headers })
+      .pipe(
+        map(({ token, user }) => this.setAuth(user, token)),
+        catchError(() => {
+          this._authStatus.set(AuthStatus.notAuthenticated);
+          return of(false);
+        })
       );
   }
 }
